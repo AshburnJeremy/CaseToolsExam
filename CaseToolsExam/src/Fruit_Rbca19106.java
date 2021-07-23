b94d23c9ca6d13c85a03993b4cdd123ec1e97584
ashburn <ashburn@LAPTOP-5KTAUTCE>
2021-07-23 14:20:26 +0530

    Second Program

--- CaseToolsExam/src/Fruit_Rbca19106.java
+++ CaseToolsExam/src/Fruit_Rbca19106.java
@@ -5,7 +5,10 @@ public class Fruit_Rbca19106 {
 	{
 	this.color=color;
 	}
-	
+	String get_color()
+	{
+	return color;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
